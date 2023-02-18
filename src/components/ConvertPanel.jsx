553654534514fbec048635bf63183b88b3d3a31f
Fernando Requena <fernandorequenaserra@gmail.com>
2023-02-18 13:16:57 +0100

    100% funcional

--- src/components/ConvertPanel.jsx
+++ src/components/ConvertPanel.jsx
@@ -1,43 +1,67 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 import { TbArrowsLeftRight } from "react-icons/tb";
+import nextId from "react-id-generator";
 
 export default function ConvertPanel() {
-  const [conversion, setConversion] = useState("km → miles");
-  const [uniti, setUniti] = useState("miles");
-  const [inputValor, setInputValor] = useState(0);
+  const [conversion, setConversion] = useState(
+    JSON.parse(localStorage.getItem("conversion")) || "km → miles"
+  );
+  const [uniti, setUniti] = useState(
+    JSON.parse(localStorage.getItem("uniti")) || "miles"
+  );
+  const [uniti2, setUniti2] = useState(
+    JSON.parse(localStorage.getItem("uniti2")) || "km"
+  );
+  const [inputValor, setInputValor] = useState(
+    JSON.parse(localStorage.getItem("inputValor")) || 0
+  );
+  const listLocal = JSON.parse(localStorage.getItem("list")) || [];
+  const [list, setList] = useState(listLocal || []);
+
   const [valor, setValor] = useState(inputValor);
+  localStorage.setItem("conversion", JSON.stringify(conversion));
+  localStorage.setItem("inputValor", JSON.stringify(inputValor));
+  let htmlId = nextId();
 
   useEffect(() => {
     switch (conversion) {
       case "km → miles":
         setValor(0.62137 * inputValor);
         setUniti("miles");
+        setUniti2("km");
         break;
       case "miles → km":
         setValor(1.60934 * inputValor);
         setUniti("km");
+        setUniti2("miles");
         break;
       case "feeds → meters":
         setValor(0.3048 * inputValor);
         setUniti("meters");
+        setUniti2("feeds");
         break;
       case "meters → feeds":
         setValor(3.28084 * inputValor);
         setUniti("feeds");
+        setUniti2("meters");
         break;
       case "cm → inches":
         setValor(0.393701 * inputValor);
         setUniti("inches");
+        setUniti2("cm");
         break;
       case "inches → cm":
         setValor(2.54 * inputValor);
         setUniti("cm");
+        setUniti2("inches");
         break;
       default:
         break;
     }
+    localStorage.setItem("valor", JSON.stringify(valor));
+    localStorage.setItem("uniti", JSON.stringify(uniti));
+    localStorage.setItem("uniti2", JSON.stringify(uniti2));
   });
 
   const arrowFunc = () => {
@@ -61,17 +85,28 @@ export default function ConvertPanel() {
     }
     document.getElementById("inputId").value = valor.toFixed(2);
     setConversion(document.getElementById("selectId").value);
-
-    console.log(conversion);
+    localStorage.setItem("conversion", JSON.stringify(conversion));
+    setInputValor(valor.toFixed(2));
+    localStorage.setItem("inputValor", JSON.stringify(inputValor));
+    localStorage.setItem("valor", JSON.stringify(valor));
   };
 
-  const saveFunction = () => {};
+  const saveFunction = () => {
+    setList([...list, { id: htmlId, inputValor, uniti2, valor, uniti }]);
+  };
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <>
       <div className="convertPanel">
         <h1>convert</h1>
-        <select id="selectId" onClick={(e) => setConversion(e.target.value)}>
+        <select
+          defaultValue={conversion}
+          id="selectId"
+          onClick={(e) => setConversion(e.target.value)}
+        >
           <option>km → miles</option>
           <option>miles → km</option>
           <option>feeds → meters</option>
@@ -82,18 +117,31 @@ export default function ConvertPanel() {
 
         <TbArrowsLeftRight onClick={() => arrowFunc()} />
         <input
+          onChange={(e) => setInputValor(e.target.value)}
+          defaultValue={inputValor !== 0 ? inputValor : ""}
           id="inputId"
           type="number"
           placeholder="Introduzca un numero"
-          onChange={(e) => setInputValor(e.target.value)}
         />
+        {uniti2}
         <h1 className="corazone">
           <AiOutlineHeart onClick={() => saveFunction()} />
-          {valor.toFixed(2)}
+          {parseFloat(valor).toFixed(2)}
           {uniti}
         </h1>
       </div>
       <h1>saved</h1>
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>
+            {" "}
+            {item.inputValor}
+            {item.uniti2}→{item.valor}
+            {item.uniti}{" "}
+            <button onClick={(e) => console.log(e.target.value)}>X</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
